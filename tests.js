fbd56d03616bf6ab617f5cbde0b3d04aa456c554
Randall Morey <randall@randallmorey.com>
2018-02-08 15:18:15 -0500

    test(coverage): setup test coverage reporting

--- tests.js
+++ tests.js
@@ -10,7 +10,7 @@ describe('Greenmap map():', () => {
     done();
   });
 
-  it('should return a promise that asynchronously maps the passed array using the passed function', async (done) => {
+  it('should return a promise that asynchronously maps the passed array using the passed function', (done) => {
     const promise = map([1, 2, 3], (item) => ++item);
     assert.ok(promise);
     promise.then((result) => {
@@ -20,4 +20,14 @@ describe('Greenmap map():', () => {
       done();
     });
   });
+});
+
+Testem.afterTests((config, data, callback) => {
+  const coverage = JSON.stringify(window.__coverage__);
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) callback();
+  };
+  xhr.open('POST', '/coverage', true);
+  xhr.send(coverage);
 });
