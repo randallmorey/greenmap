a2ed47184d2cc28675c69c4580318b17b662279a
Randall Morey <randall@randallmorey.com>
2018-02-13 10:15:36 -0500

    test(reduce): add reduce unit tests

--- tests.js
+++ tests.js
@@ -1,4 +1,4 @@
-import { map } from './greenmap';
+import { map, reduce } from './greenmap';
 import assert from 'assert';
 
 describe('Greenmap map():', () => {
@@ -19,6 +19,29 @@ describe('Greenmap map():', () => {
       assert.equal(result[2], 4);
       done();
     });
+  });
+});
+
+describe('Greenmap reduce():', () => {
+  it('asynchronously reduces the passed array using the passed function', async (done) => {
+    const result = await reduce([1, 2, 3, 4], (accumulator, currentValue) => accumulator + currentValue);
+    assert.equal(result, 10);
+    done();
+  });
+
+  it('should return a promise that asynchronously maps the passed array using the passed function', (done) => {
+    const promise = reduce([1, 2, 3, 4], (accumulator, currentValue) => accumulator + currentValue);
+    assert.ok(promise);
+    promise.then((result) => {
+      assert.equal(result, 10);
+      done();
+    });
+  });
+
+  it('accepts an `initialValue` argument', async (done) => {
+    const result = await reduce([1, 2, 3, 4], ((accumulator, currentValue) => accumulator + currentValue), 10);
+    assert.equal(result, 20);
+    done();
   });
 });
 
